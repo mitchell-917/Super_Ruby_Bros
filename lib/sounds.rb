b94d66ea1f597d9399ff0ef9a72eb7044aeef7ff
mitchell-917 <mitchell.perry001@gmail.com>
2021-04-03 15:19:27 +0100

    Updated Sound Locations

--- lib/sounds.rb
+++ lib/sounds.rb
@@ -1,10 +1,10 @@
 class Sounds
   
   def initialize
-    @coin = Sound.new('./assets/coin.wav')
-    @game_over = Sound.new('./assets/game_over.wav')
-    @death = Sound.new('./assets/death.wav')
-    @next_level = Sound.new('./assets/next_level.wav')
+    @coin = Sound.new('C:\Users\Desktop-01\Documents\VS_CODE_Projects\Super_Ruby_Bros\assets\coin.wav')
+    @game_over = Sound.new('C:\Users\Desktop-01\Documents\VS_CODE_Projects\Super_Ruby_Bros\assets\game_over.wav')
+    @death = Sound.new('C:\Users\Desktop-01\Documents\VS_CODE_Projects\Super_Ruby_Bros\assets\death.wav')
+    @next_level = Sound.new('C:\Users\Desktop-01\Documents\VS_CODE_Projects\Super_Ruby_Bros\assets\next_level.wav')
   end
 
   def coin
